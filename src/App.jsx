b33a4455103e2d13ca1818cbcdaca3678553e9c2
polar <joao.basa.18@gmail.com>
2022-03-10 00:27:22 -0500

    Limpieza de Código

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Polar from './components/polar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>a</h1>
+      <Polar></Polar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/polar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Polar(){
+    return(
+        <div>
+            <h1>Polar</h1>
+        </div>
+    )
+}
+
+export default Polar;
